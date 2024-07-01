e3851135a365f2f2e48328e977fbad46b57fb83b
Ben <benpersitz@airship.com>
2024-07-01 16:16:38 -0700

    v2

--- assets/SDK_functions.js
+++ assets/SDK_functions.js
@@ -1,31 +1,67 @@
 // SDK CODES
 
-function prompt_registration() {
-	UA.then((sdk) => {
-		sdk.plugins
-			.load(
-				"html-prompt",
-				"https://aswpsdkus.com/notify/v2/ua-html-prompt.min.js",
-				{
-					askAgainDelay: 0,
-					stylesheet: "assets/airshipstyles.css",
-					i18n: {
-						en: {
-							title: "Lets keep in touch",
-							message:
-								"Make sure you stay up to date with everything Airship SDK. Subscribe to notifications. We send about one per week.",
-							accept: "Sign up now",
-							deny: "So you're saying there's a chance.."
-						}
-					}
-				}
-			)
-			.then((plugin) => plugin.prompt());
-	});
+// Log who you are in the console
+async function whoAmI() {
+    const sdk = await UA
+	const channel = await sdk.channel.id()
+    console.log(channel)
 }
 
-function prompt_sms_form() {
-	let options = {
+
+
+async function promptRegistration() {
+    const sdk = await UA
+	// UA.then((sdk) => {
+	// 	sdk.plugins
+	// 		.load(
+	// 			"html-prompt",
+	// 			"https://aswpsdkus.com/notify/v2/ua-html-prompt.min.js",
+	// 			{
+	// 				appearDelay: 0,
+	// 				askAgainDelay: 0,
+	// 				type: "alert",
+	// 				position: "top",
+	// 				i18n: {
+	// 					en: {
+	// 						title: "Lets keep in touch",
+	// 						message:
+	// 							"Make sure you stay up to date with everything Airship SDK. Subscribe to notifications. We send about one per week.",
+	// 						accept: "Sign up now",
+	// 						deny: "So you're saying there's a chance.."
+	// 					}
+	// 				}
+	// 			}
+	// 		)
+	// 		.then((plugin) => {
+	// 			plugin.prompt();
+	// 		})
+	//     });
+	;
+	const plugin = await sdk.plugins.load(
+		"html-prompt",
+		"https://aswpsdkus.com/notify/v2/ua-html-prompt.min.js",
+		{
+			appearDelay: 0,
+			askAgainDelay: 0,
+			type: "alert",
+			position: "top",
+			i18n: {
+				en: {
+					title: "Lets keep in touch",
+					message:
+						"Make sure you stay up to date with everything Airship SDK. Subscribe to notifications. We send about one per week.",
+					accept: "Sign up now",
+					deny: "So you're saying there's a chance.."
+				}
+			}
+		}
+	);
+	plugin.prompt();
+}
+.
+async function prompt_sms_form() {
+    const sdk = await UA
+    let options = {
 		platform: "sms",
 		// automatic: {
 		// 	askAgainDelay: 0
@@ -42,18 +78,15 @@ function prompt_sms_form() {
 		senderId: "63706",
 		country: "US"
 	};
-	UA.then((sdk) => {
-		return sdk.plugins.load(
-			"subscription-form",
-			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js"
-		);
-	}).then((formFactory) => {
-		let form = formFactory.setupModalForm(options);
-		form.open();
-	});
+    
+    const plugin = await sdk.plugins.load("subscription-form", "https://aswpsdkus.com/notify/v2/ua-subscription-form.min.js")
+    form = plugin.setupModalForm(options)
+    form.open()
+
+    // })
 }
 
-function prompt_email_form() {
+async function prompt_email_form() {
 	let options = {
 		platform: "email",
 		size: "large",
@@ -72,133 +105,55 @@ function prompt_email_form() {
 		senderId: "63706",
 		country: "US"
 	};
-	UA.then((sdk) => {
-		return sdk.plugins.load(
-			"subscription-form",
-			"https://aswpsdkus.com/notify/v1/ua-subscription-form.min.js"
-		);
-	}).then((formFactory) => {
-		formFactory.setupModalForm(options);
-		// form.open()  //call this if you aren't using automatic display
-	});
+    
+    const plugin = await sdk.plugins.load("subscription-form", "https://aswpsdkus.com/notify/v2/ua-subscription-form.min.js")
+    form = plugin.setupModalForm(options)
+    form.open()
 }
 
 async function associateNamedUser() {
-	const namedUserId = document.querySelector("#nuid").value;
-	const SDK = await UA;
-	const channel = await SDK.getChannel();
-	channel.namedUser.set(namedUserId);
+    const sdk = await UA
+    const nuValue = document.querySelector("#nuid").value
+    const contact = await sdk.contact
+    contact.identify(nuvalue)
 }
 
-async function addTags() {
-	let result = false;
+function addTags() {
 	const tagNU = document.getElementById("tagNU");
-	const tagGroup = document.getElementById("tag-group").value;
-	const tagString = document.getElementById("tag-name").value;
-	let tagArray = tagString.split(",");
-	tagArray = tagArray.map((tag) => tag.trim())
-	const SDK = await UA;
-	const channel = await SDK.getChannel();
-	for (let tag of tagArray) {
-		if (tagNU.checked) {
-			result = await channel.namedUser.tags.add(tag, tagGroup);
-		} else {
-			result = await channel.tags.add(tag, tagGroup);
-		}
-	}
-	notifyResult(result);
-}
 
-async function removeTags() {
-	let result = false;
-	const tagNU = document.getElementById("tagNU");
-	const tagGroup = document.getElementById("tag-group").value;
-	const tagString = document.getElementById("tag-name").value;
-	let tagArray = tagString.split(",");
-	tagArray = tagArray.map((tag) => tag.trim())
-	const SDK = await UA;
-	const channel = await SDK.getChannel();
-	for (let tag of tagArray) {
-		if (tagNU.checked) {
-			result = await channel.namedUser.tags.remove(tag, tagGroup);
-		} else {
-			result = await channel.tags.remove(tag, tagGroup);
-		}
-	}
-	notifyResult(result);
-}
-
-async function setTags() {
-	const SDK = await UA;
-	const channel = await SDK.getChannel();
-	let result = false;
-	const tagNU = document.getElementById("tagNU");
-	const tagGroup = document.getElementById("tag-group").value;
-	const tagString = document.getElementById("tag-name").value;
-	let tagArray = tagString.split(",");
-	const trimmedTagArray = tagArray.map((tag) => tag.trim())
 	if (tagNU.checked) {
-		result = await channel.namedUser.tags.set(trimmedTagArray, tagGroup);
+		// Add SDK codes to add tags on Named User
 	} else {
-		result = await channel.tags.set(trimmedTagArray, tagGroup);
+		// Add SDK codes to add tags on Channel
 	}
-	notifyResult(result);
 }
 
-async function setAttrs() {
+function removeTags() {
+	const tagNU = document.getElementById("tagNU");
+
+	if (tagNU.checked) {
+		// Add SDK codes to remove tags on Named User
+	} else {
+		// Add SDK codes to remove tags on Channel
+	}
+}
+
+function setTags() {
+	const tagNU = document.getElementById("tagNU");
+
+	if (tagNU.checked) {
+		// Add SDK codes to set tags on Named User
+	} else {
+		// Add SDK codes to set tags on Channel
+	}
+}
+
+function setAttrs() {
 	const attrNU = document.getElementById("attrNU");
-	const attrForm = document.querySelector("#attr-form");
-	const SDK = await UA;
-	const channel = await SDK.getChannel();
-	const fnValue = document.querySelector("#first_name").value;
-	const lnValue = document.querySelector("#last_name").value;
-	const tierValue = document.querySelector("#loyalty_tier").value;
-	let valueList = {
-		first_name: fnValue,
-		last_name: lnValue,
-		loyalty_tier: tierValue
-	};
-	for (let value of Object.keys(valueList)) {
-		if (valueList[value] == "") {
-			delete valueList[value];
-		} else if (valueList[value] == "null") {
-			valueList[value] = "";
-		}
-	}
+    console.log('wow')
 	if (attrNU.checked) {
-		const result = await channel.namedUser.attributes.set(valueList);
-		notifyResult(result);
+		// Add SDK codes to set attributes on Named User
 	} else {
-		const result = await channel.attributes.set(valueList);
-		notifyResult(result);
-	}
-}
-
-function notifyResult(result) {
-	const tagForm = document.querySelector("#tags-form");
-	if (result == true) {
-		console.log("tag operation completed successfully");
-		Toastify({
-			text: "Tag operation completed successfully",
-			duration: 5000,
-			className: "success-toast",
-			position: "center",
-			close: true,
-			style: {
-				background: "green"
-			}
-		}).showToast();
-		tagForm.reset();
-	} else {
-		Toastify({
-			text: "Error completing the tag operation",
-			duration: 5000,
-			className: "error-toast",
-			position: "center",
-			close: true,
-			style: {
-				background: "red"
-			}
-		}).showToast();
+		// Add SDK codes to set attributes on Channel
 	}
 }
