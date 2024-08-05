1f77a695eb9b088918275cb6a6e7f17e871f4664
Ben <benpersitz@airship.com>
2024-08-05 09:16:47 -0700

    updating app key

--- push-worker.js
+++ push-worker.js
@@ -1,10 +1,11 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1715970619.5832934
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1722874445.410077
 importScripts('https://aswpsdkus.com/notify/v2/ua-sdk.min.js')
 uaSetup.worker(self, {
-  defaultIcon: 'https://archive.smashing.media/assets/344dbf88\u002Dfdf9\u002D42bb\u002Dadb4\u002D46f01eedd629/d0a4481f\u002De801\u002D4cb7\u002D9daa\u002D17cdae32cc89/icon\u002Ddesign\u002D21\u002Dopt.png',
-  defaultTitle: 'benjaminpersitz.dev',
-  defaultActionURL: 'https://bpaonsite.surge.sh',
-  appKey: 'G0MM8eAYR92mbaosIHxz6g',
-  token: 'MTpHME1NOGVBWVI5Mm1iYW9zSUh4ejZnOnBhMlpOaWdfTGRpS3RQel9yR0RTQ29LVWR3RFg2VlJNVVV5alprR2dqTEE',
-  vapidPublicKey: 'BGoaq6OB072RABcfTdWex-Cg60IBjDT9kJPGWGB6jrzSYgY6T-B6TEC3PKnlpbVXALnijOMMoKfz6lWvqiPIgeI='
+  
+  defaultIcon: 'https://shippie.surge.sh//assets/img/jean\u002Djacques\u002Dshippie.png',
+  defaultTitle: 'Airship Web Demo',
+  defaultActionURL: 'https://websdkv2.onrender.com',
+  appKey: 'aAilG4xkRGa85IsYEauOTg',
+  token: 'MTphQWlsRzR4a1JHYTg1SXNZRWF1T1RnOjc0NF9PbGQ1UlNFeWpnUE01b1N4azducFdQOUNZMXBudFJkWkhCcGwzaFk',
+  vapidPublicKey: 'BE1ERiAuCpj6ebcnwL5DZcdoerjq_Dd3K_fEKInQkxA196jgP9GFTbaLZbZikeSLa3RcCNpJ4pXVVVIJAoXaDcE='
 })
