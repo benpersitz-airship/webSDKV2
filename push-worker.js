d5bfd3b1adbecacd5fc9aaf0345d6b5fb2f5f6c6
Ben <benpersitz@airship.com>
2025-01-27 15:16:42 -0800

    updating app key

--- push-worker.js
+++ push-worker.js
@@ -1,11 +1,11 @@
-// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1722874445.410077
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1738019730.9504967
 importScripts('https://aswpsdkus.com/notify/v2/ua-sdk.min.js')
 uaSetup.worker(self, {
   
-  defaultIcon: 'https://shippie.surge.sh//assets/img/jean\u002Djacques\u002Dshippie.png',
-  defaultTitle: 'Airship Web Demo',
+  defaultIcon: 'https://hangar\u002Ddl.urbanairship.com/binary/public/299rBFDoT2qfFMfKJxansg/4e62e119\u002D237d\u002D488f\u002Dbe5a\u002D6e7b3ad2dc2b',
+  defaultTitle: 'Neptune\u0027s Sandbox',
   defaultActionURL: 'https://websdkv2.onrender.com',
-  appKey: 'aAilG4xkRGa85IsYEauOTg',
-  token: 'MTphQWlsRzR4a1JHYTg1SXNZRWF1T1RnOjc0NF9PbGQ1UlNFeWpnUE01b1N4azducFdQOUNZMXBudFJkWkhCcGwzaFk',
-  vapidPublicKey: 'BE1ERiAuCpj6ebcnwL5DZcdoerjq_Dd3K_fEKInQkxA196jgP9GFTbaLZbZikeSLa3RcCNpJ4pXVVVIJAoXaDcE='
+  appKey: '299rBFDoT2qfFMfKJxansg',
+  token: 'MToyOTlyQkZEb1QycWZGTWZLSnhhbnNnOkhnaVRTREszRHlGNTN6aU9sbm1FRmdfWXNqMnQ0QWV3RVNyNUc2YVo2bVE',
+  vapidPublicKey: 'BMKFnPRtBmA5GaeYN66G46Lvkxz-uOtRF9gyC9InS9byiMlzXD8oK83swlLdW-m4MQYZCE2X07SUa8xYmUwv60s='
 })
