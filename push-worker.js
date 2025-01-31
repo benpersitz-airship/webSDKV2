fc984182c2a5ca40c8b52923504b8a3f5e0eb4e4
Ben <benpersitz@airship.com>
2025-01-31 08:12:53 -0800

    debug

--- push-worker.js
+++ push-worker.js
@@ -5,6 +5,7 @@ uaSetup.worker(self, {
   defaultIcon: 'https://hangar\u002Ddl.urbanairship.com/binary/public/299rBFDoT2qfFMfKJxansg/4e62e119\u002D237d\u002D488f\u002Dbe5a\u002D6e7b3ad2dc2b',
   defaultTitle: 'Neptune\u0027s Sandbox',
   defaultActionURL: 'https://websdkv2.onrender.com',
+  debug: true,
   appKey: '299rBFDoT2qfFMfKJxansg',
   token: 'MToyOTlyQkZEb1QycWZGTWZLSnhhbnNnOkhnaVRTREszRHlGNTN6aU9sbm1FRmdfWXNqMnQ0QWV3RVNyNUc2YVo2bVE',
   vapidPublicKey: 'BMKFnPRtBmA5GaeYN66G46Lvkxz-uOtRF9gyC9InS9byiMlzXD8oK83swlLdW-m4MQYZCE2X07SUa8xYmUwv60s='
